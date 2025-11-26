ef32574837574eb67c480855365a2d24064f8715
GLADYANDREWS <andrewsglady2004@gmail.com>
2025-11-26 20:49:43 +0530

    Update vite.config.ts for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/", // for github pages with custom domain use "/"
+  base: "./", // for github pages with custom domain use "/"
   plugins: [react()],
   resolve: {
     alias: {
