817a2d24c1f19c0b2cec9765e97c076ad94ffed3
GLADYANDREWS <andrewsglady2004@gmail.com>
2025-11-26 20:58:26 +0530

    Your commit message

--- vite.config.ts
+++ vite.config.ts
@@ -19,17 +19,19 @@
 // }));
 
 
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "./", // for github pages with custom domain use "/"
+  base: "/", // For Vercel deployment, keep it "/"
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
     }
+  },
+  build: {
+    outDir: "dist"
   }
 });
